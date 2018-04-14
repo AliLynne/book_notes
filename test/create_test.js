c80d95d23b3c15c30a453d0df4ee25dedd20c071
AliLynne <alilynne.thompson@gmail.com>
2018-04-14 13:40:04 -0700

    set up create test

--- test/create_test.js
+++ test/create_test.js
@@ -4,8 +4,14 @@ const Book = require('../src/book')
 
 describe('Creating records', () => {
 
-  it('saves a book', () => {
-    const newBook = new Book({ title: 'Book Title'})
+  it('saves a book', (done) => {
+    const testBook = new Book({ title: 'Book Title'})
+
+    testBook.save()
+      .then(() => {
+        assert(!testBook.isNew)
+        done()
+      })
   })
 
 })
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/books_test')
-mongoose.connection
-  .once('open', () => console.log('Connected'))
-  .on('error', (error) => {
-    console.warn('Warning', error)
+mongoose.Promise = global.Promise
+
+before((done) => {
+
+  mongoose.connect('mongodb://localhost/books_test')
+  mongoose.connection
+    .once('open', () => { done() })
+    .on('error', (error) => {
+      console.warn('Warning', error)
+    })
+
+})
+
+
+
+beforeEach((done) => {
+  mongoose.connection.collections.books.drop(() => {
+    done()
   })
+})
